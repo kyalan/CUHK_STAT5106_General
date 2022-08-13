933dabf0ba6057d584d73faef6e2d95b9ab77ca9
KY Alan Lo <kyalan.2013@gmail.com>
2022-08-13 16:20:29 +0800

    Add files via upload

---
+++ GeoAPI.py
@@ -0,0 +1,46 @@
+class geographer():
+    
+    api_key = None
+    
+    def __init__(self):
+        self.api_key = input('Please input Google MAP API key:')
+
+    def getGeojs(self, address, verbose=False):
+
+        import urllib.request, urllib.parse, urllib.error
+        import json
+        import ssl
+
+        serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+        # Ignore SSL certificate errors
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+        if len(address) < 1: 
+            print('==== Failure To Retrieve ====')
+            print('Address is None Value.')
+            return {}        
+
+        parms = {}
+        parms['address'] = address
+        parms['key'] = self.api_key
+        url = serviceurl + '?' + urllib.parse.urlencode(parms)
+
+        if verbose: print('Retrieving', url)
+        uh = urllib.request.urlopen(url, context=ctx)
+        data = uh.read().decode()
+        if verbose: print('Retrieved', len(data), 'characters')
+
+        try:
+            js = json.loads(data)
+        except:
+            js = None
+
+        if not js or 'status' not in js or js['status'] != 'OK':
+            print('==== Failure To Retrieve ====')
+            print(data)
+            return {}
+
+        return js
